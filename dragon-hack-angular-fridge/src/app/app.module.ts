a1d5fd8b925cea565cbc99c98d3e7ec9fb2b5489
lucashj <lh3973@student.uni-lj.si>
2022-05-14 20:30:41 +0200

    fridge mobile and web frontend

--- dragon-hack-angular-fridge/src/app/app.module.ts
+++ dragon-hack-angular-fridge/src/app/app.module.ts
@@ -7,19 +7,27 @@ import {TabMenuModule} from "primeng/tabmenu";
 import { MyFridgeComponent } from './features/my-fridge/my-fridge.component';
 import {HttpClientModule} from "@angular/common/http";
 import {CommonModule} from "@angular/common";
+import {ListboxModule} from "primeng/listbox";
+import {FormsModule} from "@angular/forms";
+import {RippleModule} from "primeng/ripple";
+import {ButtonModule} from "primeng/button";
 
 @NgModule({
   declarations: [
     AppComponent,
     MyFridgeComponent
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        TabMenuModule,
-        HttpClientModule,
-        CommonModule
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    TabMenuModule,
+    HttpClientModule,
+    CommonModule,
+    ListboxModule,
+    FormsModule,
+    RippleModule,
+    ButtonModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- dragon-hack-angular-fridge/src/app/features/my-fridge/my-fridge.component.ts
+++ dragon-hack-angular-fridge/src/app/features/my-fridge/my-fridge.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 import {FridgeService} from "../../services/fridge.service";
 import {Item} from "../../models/item";
+import {ListboxModule} from 'primeng/listbox';
 
 @Component({
   selector: 'app-my-fridge',
@@ -8,19 +9,36 @@ import {Item} from "../../models/item";
   styleUrls: ['./my-fridge.component.scss']
 })
 export class MyFridgeComponent implements OnInit {
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.checkMobile()
+  }
+  isMobile = false;
+  ingredients: Item[];
 
+
+  selectedCountries: any[] = [];
   showClosedFridge = true;
   showOpenFridge = false;
 
-  item : Item = {
+  item: Item = {
     experationDate: new Date(),
     name: 'banana',
     id: 5
   }
 
-  constructor(private fridgeService: FridgeService) { }
+  constructor(private fridgeService: FridgeService) {
+    this.ingredients = [
+      {name: "Banana", experationDate: new Date()},
+      {name: "OatMeal", experationDate: new Date()},
+      {name: "Milk", experationDate: new Date()},
+      {name: "Puding", experationDate: new Date()},
+      {name: "Fuckery", experationDate: new Date()},
+    ];
+  }
 
   ngOnInit(): void {
+    this.checkMobile()
     this.fridgeService.store(this.item).subscribe()
     this.fridgeService.getAll().subscribe(res => console.log(res))
   }
@@ -30,4 +48,7 @@ export class MyFridgeComponent implements OnInit {
     this.showOpenFridge = !this.showOpenFridge;
   }
 
+  checkMobile() {
+    this.isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+  }
 }
