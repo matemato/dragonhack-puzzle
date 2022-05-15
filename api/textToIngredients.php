b097b9551bc681dad5895b7a70975b450aa36c18
aljaz <aj2092@student.uni-lj.si>
2022-05-15 06:16:36 +0200

    mico

--- api/textToIngredients.php
+++ api/textToIngredients.php
@@ -21,7 +21,7 @@ function saveResultsToBase($data){
 
 function detect_food_in_text($text, $apiKey){
     $text = "date 06/01/2016 wed *** $4.66 zuchinni green 0.778kg net @ $5.99/kg banana cavendish 0.442kg net @ $2.99/kg special special potatoes brushed 1.328kg net @ $2.99/kg broccoli 0.808kg net @ $5.99/kg brussel sprouts 0.322kg net @ $15.99/kg special grapes green 1.174kg net @ $5.99/kg peas snow 0.218kg net @ $14.99/kg tomatoes grape lettuce iceberg subtotal loyalty subtotal subtotal $1.32 $0.99 $1.50 $3.97 $4.84 $5.15 $0.99 $7.03 $3.27 $2.99 $2.49 $39.20 - 15.00 $24.20 $24.20 subtotal total cash change $24.20 $24.20 $50.00 $25.80";
-    $url = "https://api.spoonacular.com/food/detect?apiKey=f17755694ab8485db5676b880d16dae9&text=";
+    $url = "https://api.spoonacular.com/food/detect?apiKey=1215dfe2d3a54e72ae07d0e88f7f5f7c&text=";
     #$url = $url.=$apiKey;
     #$url = $url.="&text=";
     $url = $url.=$text;
