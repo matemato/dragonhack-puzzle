c13bebb138c46a6b0c7f53ec1da2ad6720240999
aljaz <aj2092@student.uni-lj.si>
2022-05-15 09:25:02 +0200

    luka

--- api/displayStatistics.php
+++ api/displayStatistics.php
@@ -5,27 +5,28 @@ $db = Connect2::getInstance();
 try{
     $statement = $db->prepare("SELECT * FROM statistika");
     $statement->execute();
-    $res = $statement->fetchAll();    
+    $res = $statement->fetchAll();  
+    $tmpA = array();  
     if(isset($res[0]) && !empty($res[0])){
         $sum = 0;
-        $tmp = array();
+        
         foreach($res as $r){            
             $tmp = $r["UNITS"]*$r["PRICE_USED"]; 
             $sum+=$tmp;      
             // echo '
             // You have saved: '.$tmp.' with succesfully using '.$r["NAME_USED"].' before expiration.
             // ';
-            $tmp = array_push($tmp, '
+            array_push($tmpA, '
             You have saved: '.$tmp.' with succesfully using '.$r["NAME_USED"].' before expiration.
             ');
         }
-        $tmp = array_push($tmp, $sum);
+        array_push($tmpA, $sum);
     }else{
         echo'
         You have not used any items before expiration date, yet. Try better.
         ';
     } 
-    echo $tmp;   
+    print_r ($tmpA);   
     
 }catch (Exception $e) {
     echo 'Caught exception: ',  $e->getMessage(), "\n";
